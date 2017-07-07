528c6d60821eb856f9b616f97da60ca9272e5457
administrator <liquidnuker@users.noreply.github.com>
2017-07-07 15:14:23 -0700

    initial commit

---
+++ src/vue-components/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <!-- ListCollapse1 mount -->
+    <div id="listCollapse1_mount">
+    </div>
+    <!-- end ListCollapse1 mount -->
+  </div>
+</template>
+<script>
+  export default {
+    data () {
+      return {    
+      }
+    },
+    mounted: function () {
+      this.mountListCollapse();
+    },
+    methods: {
+      mountListCollapse: function() {
+        const ListCollapse1 = resolve => {
+          require.ensure(['ListCollapse1.vue'], () => {
+            resolve(require('ListCollapse1.vue'))
+          })
+        };
+        inject("#ListCollapse1_mount", ListCollapse1);
+      }
+    }
+  }
+</script>
---
+++ src/vue-components/ListCollapse1.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    ListCollapse.vue
+  </div>
+</template>
+<script>
+  export default {
+    data () {
+      return {    
+      }
+    },
+    mounted: function () {
+      console.log("ListCollapse1.vue mounted");
+    }
+  }
+</script>
